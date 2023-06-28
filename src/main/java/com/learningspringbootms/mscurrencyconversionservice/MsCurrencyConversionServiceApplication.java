7747d73330c4b320920d3e5e27d14aee7ef1f255
adhito909 <adhito909@gmail.com>
2023-06-28 22:56:28 +0700

    Create initial base ms-currency-conversion-service source code

---
+++ src/main/java/com/learningspringbootms/mscurrencyconversionservice/MsCurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learningspringbootms.mscurrencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningspringbootms/mscurrencyconversionservice/MsCurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringbootms.mscurrencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
